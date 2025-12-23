bb9727af6dde830bb5031a7b725277c45d314fc4
fasokh <fatemeh.sokhandan@gmail.com>
2025-12-23 11:36:19 +0330

    register page updated

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,19 +1,42 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "@/src/context/AuthContext";
+import { useRouter } from "next/router";
 
 const register = () => {
   const { loading, user, signup } = useAuth();
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const route = useRouter();
+
+  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const confirmPasswordHander = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const confirmPassword = e.target.value;
+    if (confirmPassword !== password) {
+      setError("رمزهای عبور مطابقت ندارند.");
+    } else {
+      setError("");
+    }
+  };
 
   const registerHandler = async (e: React.FormEvent) => {
     e.preventDefault();
+    setError("");
 
     try {
       setIsLoading(true);
-      await signup("email", "password");
+      await signup(email, password);
+      route.push("/dashboard");
     } catch (err: any) {
       switch (err.code) {
         case "auth/email-already-in-use":
@@ -35,6 +58,12 @@ const register = () => {
 
   if (loading) return <div>Loading...</div>;
 
+  useEffect(() => {
+    if (user) {
+      route.push("/dashboard");
+    }
+  }, [user, route]);
+  
   return (
     <div className="p-20 flex justify-center items-center">
       <div className="flex flex-col">
@@ -46,18 +75,20 @@ const register = () => {
           <input
             type="text"
             placeholder="ایمیل"
+            onChange={emailHandler}
             className="outline-none border border-gray-500 rounded-l"
           />
           <input
             type="password"
             placeholder="رمز عبور"
+            onChange={passwordHandler}
             className="outline-none border border-gray-500 rounded-l"
           />
           <input
             type="password"
             name="confirmPassword"
-            id=""
             placeholder="تکرار رمز عبور"
+            onChange={confirmPasswordHander}
             className="outline-none border border-gray-500 rounded-l"
           />
           <button
