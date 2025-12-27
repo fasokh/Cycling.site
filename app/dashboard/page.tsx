f8cb00257c36eff58fab4fa92199f37c1f6282f9
fasokh <fatemeh.sokhandan@gmail.com>
2025-12-27 22:11:19 +0330

    dashboard page updated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,15 @@
 "use client";
 
 import ProtectedRout from "@/src/components/ProtectedRoute";
-import MenuUserSection from "@/src/components/header/UserMenu";
 import { useAuth } from "@/src/context/AuthContext";
+import DashboardContent from "@/src/components/dashboard/DashboardContent";
 
 const Dashboard = () => {
   const { user } = useAuth();
 
   return (
     <ProtectedRout>
-      <div className="w-full flex flex-col gap-4 p-8">
-        <p>سلام{user?.displayName}</p>
-        <p>به پنل کاربری خوش آمدید</p>
-        <div>
-          <ul>
-            <li>مسیرهای دوچرخه سواری</li>
-            <li>مسیر جدید</li>
-            <li>آمار رکاب زدن</li>
-          </ul>
-        </div>
-      </div>
+      <DashboardContent />
     </ProtectedRout>
   );
 };
